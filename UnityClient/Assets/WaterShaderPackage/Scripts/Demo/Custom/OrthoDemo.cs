15a14040a2afe0cfb6b6f4c89706de553299d495
Colin Cove <ccove@pandeagames.com>
2020-03-30 21:45:55 -0700

    adding to visuals of games

---
+++ UnityClient/Assets/WaterShaderPackage/Scripts/Demo/Custom/OrthoDemo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OrthoDemo : MonoBehaviour
+{
+	public Material mat;
+	public string sceneName;
+
+	public void OnEdgeChanged(float newValue)
+	{
+		mat.SetFloat("_EdgeFade", newValue);
+	}
+
+	public void OnNormalStrengthChanged(float newValue)
+	{
+		mat.SetFloat("_NormalStrength", newValue);
+	}
+
+	public void OnLightOffsetChanged(float newValue)
+	{
+		mat.SetFloat("_LightOffset", newValue);
+	}
+
+	public void OnReflectivityChanged(float newValue)
+	{
+		mat.SetFloat("_Reflectivity", newValue);
+	}
+
+	public void NextScene()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
